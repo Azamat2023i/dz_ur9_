f1a30ef881ddec6290f740b4783e9cc5a2a32563
Azamat2023i <azamatilbulov@gmail.com>
2023-08-18 19:08:29 +0500

    first commit

---
+++ advertisements/app_advertisements/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import Advertisement
+from django.utils.html import format_html
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date', 'auction', 'image_thumbnail']
+    list_filter = ['price', 'created_at', 'updated_at', 'auction']
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+    fieldsets = (
+        ('Общие', {'fields': ('title', 'description', 'image', 'user')}),
+        ('Финансы', {'fields': ('price', 'auction')})#, 'classes': ['collapse']})
+    )
+     
+    
+    
+    
+    
+    
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+    
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+
+    @admin.display(description='Изображение')
+    def image_thumbnail(self, obj):
+        return format_html('<img src="{}" width="50" height="50" />'.format(obj.image.url))
+
+
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+ 
---
+++ advertisements/app_advertisements/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Advertisement
+
+def index(request):
+    advertisements = Advertisement.objects.all()
+    cntext = {"advertisements": advertisements}
+    return render(request, 'index.html', cntext)
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+
+
+def advertisement_post(request):
+    return render(request, 'advertisement-post.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def advertisement(request):
+    return render(request, 'advertisement.html')
+
+
+
